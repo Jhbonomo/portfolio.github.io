c80740f79427358d40fcf0d9d55bea366fc7eb15
Juliana <jholandabonomo@gmail.com>
2023-01-05 16:58:09 -0300

    about me ajsutes

--- info.js
+++ info.js
@@ -9,27 +9,29 @@ const cardsArray = [
     {
     title: "Browse and fast access journeys",
     description: "Prior to the Black Friday, we included fast access cards to present featured categories (e.g. mobile, home appliances, electronics...) on the homepage.",
-    pictureUrl: "images/via-pic_1.png",
+    pictureUrl: "images/home.png",
   },
 
   {
     title: "User location and delivery address",
     description: "Product analysis of the flow across user journeys from the user address and the location components. I monitored the behaviour through Smartlook and Google Analytics. The main problem was the lack of persistence and integration of the components across the user journey on the app.",
-    pictureUrl: "images/via-pic_1.png",
+    pictureUrl: "images/address.png",
   },
   {
     title: "Applying new Design System",
     description: "UI changes to improve accessibility over text styles, color system and other  formatting issues.", 
-    pictureUrl: "images/via-pic_1.png",
+    pictureUrl: "images/discovery.png",
   },
   {
     title: "Design documentation and reports",
-    description: "I Created data Studio reports with Google Analytics metrics to provide a monitoring interface directly related to the changes implemented by my squad. The squad process was also documented and published at Via's confluence.",   pictureUrl: "/images/via-pic_1.png",
+    description: "I Created data Studio reports with Google Analytics metrics to provide a monitoring interface directly related to the changes implemented by my squad. The squad process was also documented and published at Via's confluence.",   
+    pictureUrl: "",
   },
 
   {
     title: "Other contributions",
-    description: "UX app tribe: conversations among designers from others squads to exchange materials and talk through the desired common goals specially when the squads overlapped.<br>Support implementation: As any new change should be properly tagged to obtain user metrics, I reviewed the current tag book to properly implement Analytics on the new implementations.<br>Supported QA and Project managers: I supported my peers with the testing and validation phases, also keeping up with firebase and Jira\.<br>UX Benchmarking: I teamed up with other designers to compare Via's products with other e-commerce platforms to identify strenghts, weakness and opportunities for Via's apps.",   
+    description: "<b>UX app tribe:</b> conversations among designers from others squads to exchange materials and talk through the desired common goals specially when the squads overlapped.<br><b>Support implementation:</b> As any new change should be properly tagged to obtain user metrics, I reviewed the current tag book to properly implement Analytics on the new implementations.<br><b>Supported QA and Project managers:</b> I supported my peers with the testing and validation phases, also keeping up with firebase and Jira\.<br><b>UX Benchmarking:</b> I teamed up with other designers to compare Via's products with other e-commerce platforms to identify strenghts, weakness and opportunities for Via's apps.",   
+    pictureUrl: "",
   },
 
     
