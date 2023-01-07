a38cc8a39823d98c8c862cae9305cf24e746a39f
Juliana <jholandabonomo@gmail.com>
2023-01-07 10:01:55 -0300

    fix urls

--- pages/info.js
+++ pages/info.js
@@ -4,24 +4,24 @@ const viaArray = [
     {
     title: "Login and onboarding journeys",
     description: "We reduced the bounce rate on the login process that would affect several parts of the interface, especially the buying journey, visible on user metrics.",
-    pictureUrl: "/images/via/img-1.png",
+    pictureUrl: "..../images/via/img-1.png",
   },
     
     {
     title: "Browse and fast access journeys",
     description: "Prior to the Black Friday, we included fast access cards to present featured categories (e.g. mobile, home appliances, electronics...) on the homepage.",
-    pictureUrl: "/images/via/img-2.png",
+    pictureUrl: "..../images/via/img-2.png",
   },
 
   {
     title: "User location and delivery address",
     description: "Product analysis of the flow across user journeys from the user address and the location components. I monitored the behaviour through Smartlook and Google Analytics. The main problem was the lack of persistence and integration of the components across the user journey on the app.",
-    pictureUrl: "/images/via/img-3.png",
+    pictureUrl: "..../images/via/img-3.png",
   },
   {
     title: "Applying new Design System",
     description: "UI changes to improve accessibility over text styles, color system and other  formatting issues.", 
-    pictureUrl: "/images/via/img-4.png",
+    pictureUrl: "..../images/via/img-4.png",
   },
   {
     title: "Design documentation and reports",
@@ -39,7 +39,7 @@ const viaArray = [
 ];
 const viaBanner = [ 
   {
-  imgBanner: "/images/via/main-banner.png",
+  imgBanner: "../images/via/main-banner.png",
   bgColor: "#A043E9",
   info: "Product designer for mobile apps of three e-commerce brands: Casas Bahia, Ponto and Extra. I was part of the APP experience squad, a cross-functional team that worked together with other squads.",
   buttonTitle: "Go to PNIPE case",
@@ -51,35 +51,35 @@ const pnipeArray = [
   {
   title: "Coming up with an idea",
   description: "Seeking a more effective solution, the project manager and I included stakeholders and developers on an early stage of the prduct using Figma as a collaborative environment. This process was later reviewed and much was adapted to other projects by the National Education and Research Network.<br>We refined user journeys ideas with the developing team, also giving them more time to look into technical solutions before the story was added to their backlog. Meanwhile the same file was used with Client and users to validate ideas, strategy and to elaborate the content such as translations, images, etc.",
-  pictureUrl: "/images/pnipe/img-1.png",
+  pictureUrl: "../images/pnipe/img-1.png",
 
 },
   
   {
   title: "Specifying the interface",
   description: "I designed the interface, created assets and compiled content to deliver for backlog development. The design system, created by my UX peer at RNP, was adapted from the design guidelines from the Brazilian government.<br>As the platform evolved, I partnered up with the front-end developer to come up with new UI components based on the platform visual identity, the Government design guidelines and React's material-ui.",
-  pictureUrl: "/images/pnipe/img-2.png",
+  pictureUrl: "../images/pnipe/img-2.png",
 },
 
 {
   title: "Product strategy",
   description: "The MVP focused on Labs and Institutions users. Laboratories were required to have equipment registered on the platform in order to participate in a funding public selection. That way the platform would have an initial load for its scientific catalogue. After it was launched, we focused our efforts towards researcher users to put the platform into motion. I gathered user metrics from the platform and users feedback from the support team to analyze and propose a strategy for evolving the MVP to adapt the platform for a wider audience.",
-  pictureUrl: "/images/pnipe/img-3.png",
+  pictureUrl: "../images/pnipe/img-3.png",
 },
 {
   title: "Platform evolution",
   description: "We presented to business stakeholders the results from the 1st phase and a step-by-step proposal of new features and improvements to achieve the next project's milestones defined on the platform roadmap.", 
-  pictureUrl: "/images/pnipe/img-4.png",
+  pictureUrl: "../images/pnipe/img-4.png",
 },
   
 ];
 const pnipeBanner = [ 
   {
-  imgBanner: "/images/pnipe/main-banner.png",
+  imgBanner: "../images/pnipe/main-banner.png",
   bgColor: "#47C7D8",
   info: "PNIPE is an initiative from the Ministry of Science and Technology to gather information about resources from brazilian Institutions for Science, Technology and Innovation.",
   buttonTitle: "Go to Museum app case",
-  buttonUrl: "museu.html"
+  buttonUrl: "..museu.html"
   },]
 
 const museuArray = [ 
@@ -93,13 +93,13 @@ const museuArray = [
   {
   title: "The fire",
   description: "In 2018 the Museum was hit by a massive fire, damaging the building and the collection. Some examples of important pieces that was either damaged or extinct by the fire are Luzia, one of the oldest human remains from Americas, a 13 meter high dinosaur and an Egyptian mummy with almost 2 thousand years.<br>After the fire, the community initiated a big and long task force for rescuing everything they could in the museum. At the same time, national and international entities from the public and private sector  joined forces to come up with a long-term plan of reconstruction, sponsorship and governance for the museum.",
-  pictureUrl: "/images/museu/img-01.png",
+  pictureUrl: "../images/museu/img-01.png",
 },
 
 {
   title: "The app design",
   description: "The MVP focused on Labs and Institutions users. Laboratories were required to have equipment registered on the platform in order to participate in a funding public selection. That way the platform would have an initial load for its scientific catalogue. After it was launched, we focused our efforts towards researcher users to put the platform into motion. I gathered user metrics from the platform and users feedback from the support team to analyze and propose a strategy for evolving the MVP to adapt the platform for a wider audience.",
-  pictureUrl: "/images/museu/img-02.png",
+  pictureUrl: "../images/museu/img-02.png",
 },
 {
   title: "The problem",
@@ -120,7 +120,7 @@ const museuArray = [
 {
   title: "Goals",
   description: "I've established a focus on education for the APP in order to differ from other ones I benchmarked. The take outs from this phase were: <br>The goals of the education section of the museum, responsible for the exhibitions to the public and the educational initiatives towards formal education and science research. <br>Identify the potential users of the app: museum stakeholders, the general public, teachers from formal education and researchers.", 
-  pictureUrl: "/images/museu/img-03.png",
+  pictureUrl: "../images/museu/img-03.png",
 },
   
 {
@@ -132,11 +132,11 @@ const museuArray = [
 ];
 const museuBanner = [ 
   {
-  imgBanner: "/images/museu/main-banner.png",
+  imgBanner: "../images/museu/main-banner.png",
   bgColor: "#FF4C41",
   info: "This app design is a conceptual project for a UX course. I chose as my educational project the design of an APP for the National Museum of Rio de Janeiro. ",
   buttonTitle: "Go to Thinking with scrap case",
-  buttonUrl: "dlab.html"
+  buttonUrl: "..dlab.html"
 
 },];
 
@@ -152,12 +152,12 @@ const dlabArray = [
 {
   title: "Scrap table",
   description: "I've created an Installation of discarded materials and prototypes from the fabrication workshops. The tools were also arranged in this creative space and the materials were reused by users from the DesignLab.",
-  pictureUrl: "/images/dlab/img-01.png",
+  pictureUrl: "../images/dlab/img-01.png",
 },
 {
   title: "Prototyping workshop",
   description: "Workshop for 5 participants with the proposal to think for a permanent workstation and storage of the material in the scrap table (materials, tools and old prototypes).", 
-  pictureUrl: "/images/dlab/img-02.png",
+  pictureUrl: "../images/dlab/img-02.png",
 },
 
 {
@@ -169,13 +169,13 @@ const dlabArray = [
 {
   title: "The legacy of scrap",
   description: "The discarded materials and prototypes from older projects provided some sort of exhibition of the work being held at DesignLab. In addition, the arbitrary constraints of scrap materials would stimulate creativity. Today it is a permanent and improved space at DesignLab.", 
-  pictureUrl: "/images/dlab/img-03.png",
+  pictureUrl: "../images/dlab/img-03.png",
 },
 
 ];
 const dlabBanner = [
 {buttonTitle: "Go to ICPedu case",
-buttonUrl: "icpedu.html"}
+buttonUrl: "..icpedu.html"}
 ];
 
 const icpeduArray = [ 
@@ -183,34 +183,34 @@ const icpeduArray = [
   {
   title: "Context",
   description: "The ICPEdu platform was first lauched focusing on a single user. With the increase of use during pandemics, I was assigned to the team responsible for expanding the platform to other 2 users profiles (admin from universities and from the RNP itself). ",
-  pictureUrl: "/images/icpedu/img-01.png",
+  pictureUrl: "../images/icpedu/img-01.png",
 },
   
   {
   title: "User interviews",
   description: "I conducted a series of interviews with stakeholders from institutions identified as “user operator”.  These users are workers from the university. They operate the system and educate end-users on how to use it.<br>They valued the system for optimizing the use of resources and speeding up processes, but many efforts needed to be taken to help end-user to understand what is and how digital certificates work.",
-  pictureUrl: "/images/icpedu/img-02.png",
+  pictureUrl: "../images/icpedu/img-02.png",
 },
 
 {
   title: "Product analysis",
   description: "I've conducted an heuristic analysis to identify usability problems on the current interface. The main problem was towards the user flow. Because they didn't understand the steps, users would constantly issue new certificates and didn't know how to actually use it for signing documents. I also analysed the design system and made changes to improve the visual accessibility.",
-  pictureUrl: "/images/icpedu/img-03.png",
+  pictureUrl: "../images/icpedu/img-03.png",
 },
 {
   title: "Journeys review and prototyping",
   description: "The redesign process included validation rounds with the participation of people responsible for the business and technology involved, reviewing user journeys, content, graphic interface and system visibility. Some of more structural recommendations form the user research were presented as report to business stakeholders and later on included in the scope of the product.", 
-  pictureUrl: "/images/icpedu/img-04.png",
+  pictureUrl: "../images/icpedu/img-04.png",
 },
 
 ];
 const icpeduBanner = [ 
   {
-  imgBanner: "/images/icpedu/main-banner.png",
+  imgBanner: "../images/icpedu/main-banner.png",
   bgColor: "#FF9238",
   info: "UX research and design for ICPEdu platform, a digital certification service offered by the National Education and Research Network (RNP) to higher education institutions.",
   buttonTitle: "Go to all works",
-  buttonUrl: "/index.html"
+  buttonUrl: "../index.html"
 },]
 
 // defining variable to retrieve the html strings
--- src/navbar.js
+++ src/navbar.js
@@ -6,13 +6,13 @@ console.log(navPage)
 if(navPage != "index") {
 navBarCode = 
         `<div><a href="/index.html" style="text-decoration: none; color: white" class="nav-arrow"><div class="fas fa-arrow-left" id="icon"></div><p>All works</p></a></div>
-        <div><a href="https://www.linkedin.com/in/julianahb/" target="_blank"> <img src="/images/linkedin.svg"/> </a></div>`
+        <div><a href="https://www.linkedin.com/in/julianahb/" target="_blank"> <img src="../images/linkedin.svg"/> </a></div>`
     
     }
     else {
         navBarCode = 
-        `<div><a href="/index.html" style="text-decoration: none ;"><p>Juholanda portfolio</p></a></div>
-        <div><a href="https://www.linkedin.com/in/julianahb/" target="_blank"> <img src="images/linkedin.svg"/> </a></div>`
+        `<div><a href="../index.html" style="text-decoration: none ;"><p>Juholanda portfolio</p></a></div>
+        <div><a href="https://www.linkedin.com/in/julianahb/" target="_blank"> <img src="../images/linkedin.svg"/> </a></div>`
     
     }
     
