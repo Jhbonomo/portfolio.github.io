ab57c8535596bbe3ca8352484d6dc4ef6231a3ed
Juliana <jholandabonomo@gmail.com>
2023-03-02 10:13:34 -0300

    header - ../

--- src/header.js
+++ src/header.js
@@ -11,14 +11,24 @@ headCode =
 
 
 <title>Ju Holanda Portfolio</title>
-<link rel="stylesheet" href="../css/reset.css"/>
-<link rel="stylesheet" href="../css/style.css"/>
 <!-- Sora -->
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700;800&display=swap"
     rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">`
+
+if (document.body.id == "index"){
+    headCode = `${headCode}
+    <link rel="stylesheet" href="css/reset.css"/>
+<link rel="stylesheet" href="css/style.css"/>`
+} else {
+    headCode = `${headCode}
+    <link rel="stylesheet" href="../css/reset.css"/>
+<link rel="stylesheet" href="../css/style.css"/>`
+}
+
+
 const head = document.querySelector("head");
 head.innerHTML = headCode;
 /* Header */
