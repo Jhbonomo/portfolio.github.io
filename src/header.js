cb03db0db3f6489fac8bdc876e7018318942d6a8
Juliana <jholandabonomo@gmail.com>
2023-03-02 08:46:25 -0300

    .. por .

--- src/header.js
+++ src/header.js
@@ -11,8 +11,8 @@ headCode =
 
 
 <title>Ju Holanda Portfolio</title>
-<link rel="stylesheet" href="../css/reset.css"/>
-<link rel="stylesheet" href="../css/style.css"/>
+<link rel="stylesheet" href="./css/reset.css"/>
+<link rel="stylesheet" href="./css/style.css"/>
 <!-- Sora -->
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -27,23 +27,23 @@ if (document.body.id != "index") {
 navBarCode =
     `
 <nav id="nav-projetos">
-<a href="../index.html" class="nav-links" id="index"><img class="icon" src="../images/house-solid.svg" style="width: 24px;">
+<a href="./index.html" class="nav-links" id="index"><img class="icon" src="./images/house-solid.svg" style="width: 24px;">
 </a>
-    <a href="../pages/e-commerce.html" class ="nav-links" id="e-commerce">
+    <a href="./pages/e-commerce.html" class ="nav-links" id="e-commerce">
         <p class="nav-page">E-commerce mobile APPs</p>
     </a>
-    <a href="../pages/pnipe.html" class ="nav-links" id="pnipe">
+    <a href="./pages/pnipe.html" class ="nav-links" id="pnipe">
     <p class="nav-page">PNIPE platform</p>
 </a>
 </a>
-    <a href="../pages/museum.html" class ="nav-links" id="museum">
+    <a href="./pages/museum.html" class ="nav-links" id="museum">
         <p class="nav-page">Museum APP</p>
     </a>
-    <a href="../pages/icpedu.html" class ="nav-links" id="icpedu">
+    <a href="./pages/icpedu.html" class ="nav-links" id="icpedu">
     <p class="nav-page">ICPEdu platform</p>
 </a>
 
-    <a href="../pages/dlab.html" class ="nav-links" id="dlab">
+    <a href="./pages/dlab.html" class ="nav-links" id="dlab">
     <p class="nav-page">DesignLab</p>
 </a>
 
